1633c1dd83f09b3f57a03475cf1709eb34bc839a
Lukas <lukas.schwarz@online.de>
2020-01-31 21:57:35 +0100

    Destroyable

---
+++ Assets/Scripts/Destroyable.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[DisallowMultipleComponent]
+public class Destroyable : MonoBehaviour
+{
+    public bool IsDestroyed { get; private set; }
+
+    [SerializeField] private UnityEvent onDestroyed;
+    public event Action<Destroyable> Destroyed;
+    
+    [SerializeField] private UnityEvent onRepaired;
+    public event Action<Destroyable> Repaired;
+    
+    
+    public void Destroy()
+    {
+        if (IsDestroyed)
+        {
+            Debug.LogError($"Can't destroy {this}: It's already destroyed!");
+            return;
+        }
+
+        IsDestroyed = false;
+        Destroyed?.Invoke(this);
+        onDestroyed.Invoke();
+    }
+
+    public void Repair()
+    {
+        if (!IsDestroyed)
+        {
+            Debug.LogError($"Can't repair {this}: It's already destroyed!");
+            return;
+        }
+
+        IsDestroyed = true;
+        Repaired?.Invoke(this);
+        onRepaired.Invoke();
+    }
+}
---
+++ Assets/Scripts/SpriteSwapper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+[RequireComponent(typeof(Destroyable))]
+public class SpriteSwapper : MonoBehaviour
+{
+    [FormerlySerializedAs("RepairedSprite")] [SerializeField] private Sprite repairedSprite;
+    [FormerlySerializedAs("DestroyedSprite")] [SerializeField] private Sprite destroyedSprite;
+
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    private Destroyable _destroyable;
+
+
+    private void OnValidate()
+    {
+        if(repairedSprite == null || destroyedSprite == null || spriteRenderer == null)
+            Debug.LogError($"Sprite wrapper {this} is not setup!");
+
+        spriteRenderer.sprite = repairedSprite;
+    }
+
+    private void Awake()
+    {
+        _destroyable = GetComponent<Destroyable>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnEnable()
+    {
+        _destroyable.Destroyed += DestroyableOnDestroyed;
+        _destroyable.Repaired += DestroyableOnRepaired;
+        spriteRenderer.sprite = _destroyable.IsDestroyed
+            ? destroyedSprite
+            : repairedSprite;
+    }
+
+    private void OnDisable()
+    {
+        _destroyable.Destroyed -= DestroyableOnDestroyed;
+        _destroyable.Repaired -= DestroyableOnRepaired;
+    }
+
+    private void DestroyableOnRepaired(Destroyable obj)
+    {
+        spriteRenderer.sprite = repairedSprite;
+    }
+
+    private void DestroyableOnDestroyed(Destroyable destroyable)
+    {
+        spriteRenderer.sprite = destroyedSprite;
+    }
+}
