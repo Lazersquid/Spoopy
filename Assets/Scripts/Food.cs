e8e04513085e37fec24db9c54fe46c5ce400c7a7
Karri Ojala <karri.ojala@solenix.ch>
2020-02-01 13:19:35 +0100

    Add food script

---
+++ Assets/Scripts/Food.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class Food : MonoBehaviour
+    {
+        public int energy = 50;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            Repairer repairer = collision.gameObject.GetComponent<Repairer>();
+            if (repairer != null)
+            {
+                repairer.ChangeEnergy(energy);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
