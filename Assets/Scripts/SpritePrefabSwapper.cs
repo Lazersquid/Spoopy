f1961b5288b211f3dc7118f50fcb79e783aba975
Lukas <lukas.schwarz@online.de>
2020-02-01 23:28:14 +0100

    Added destroyed chair

---
+++ Assets/Scripts/SpritePrefabSwapper.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
+
+public class SpritePrefabSwapper : MonoBehaviour
+{
+    [SerializeField] private Sprite repairedSprite;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private GameObject brokenVariant;
+
+    private Destroyable _destroyable;
+
+
+    private void OnValidate()
+    {
+        if (repairedSprite == null || brokenVariant == null || spriteRenderer == null)
+        {
+            Debug.LogError($"Sprite wrapper {this} is not setup!");
+            return;
+        }
+
+        spriteRenderer.sprite = repairedSprite;
+        _destroyable = GetComponent<Destroyable>();
+        SyncState();
+    }
+
+    private void Awake()
+    {
+        _destroyable = GetComponent<Destroyable>();
+        spriteRenderer.sprite = repairedSprite;
+    }
+
+    private void OnEnable()
+    {
+        _destroyable.Destroyed += DestroyableOnDestroyed;
+        _destroyable.Repaired += DestroyableOnRepaired;
+        SyncState();
+    }
+
+    private void OnDisable()
+    {
+        _destroyable.Destroyed -= DestroyableOnDestroyed;
+        _destroyable.Repaired -= DestroyableOnRepaired;
+    }
+
+    private void SyncState()
+    {
+        if (_destroyable.IsDestroyed)
+            DestroyableOnDestroyed(_destroyable);
+        else
+            DestroyableOnRepaired(_destroyable);
+    }
+
+    private void DestroyableOnRepaired(Destroyable destroyable)
+    {
+        spriteRenderer.enabled = true;
+        brokenVariant.SetActive(false);
+    }
+
+    private void DestroyableOnDestroyed(Destroyable destroyable)
+    {
+        spriteRenderer.enabled = false;
+        brokenVariant.SetActive(true);
+    }
+}
